ea7d7aa542f99f0e224cc45429a3d9cddca40fde
marc jaderson <jadersonmarc@gmail.com>
2024-03-16 21:56:34 -0300

    alter name

--- tests/stock_tax_spec.rb
+++ tests/stock_tax_spec.rb
@@ -13,12 +13,10 @@ RSpec.describe StockTax do
               Operation.new({ "operation"=> "sell", "unit-cost"=> 25.0, "quantity"=> 1000 }, 0)
             ]
           ]
-
-        
             stock_taxes = transactions.map { |transaction_list| StockTax.new(transaction_list) }
             stock_taxes.each { |stock_tax| stock_tax.calculate_taxes }
             tax =  stock_taxes.map(&:tax)
-           expect(tax).to eq([[[{:tax=>"0.00"}], [{:tax=>"0.00"}], [{:tax=>"0.00"}], [{:tax=>"1400.00"}]]])
+            expect(tax).to eq([[[{:tax=>"0.00"}], [{:tax=>"0.00"}], [{:tax=>"0.00"}], [{:tax=>"1400.00"}]]])
       end
 
       it 'calculates taxes considering accumulated loss' do
@@ -28,13 +26,10 @@ RSpec.describe StockTax do
             Operation.new({ "operation" => "sell", "unit-cost" => 5.0, "quantity" => 5000 }, 0) # Venda com prejuízo
           ]
         ]
-    
         stock_taxes = transactions.map { |transaction_list| StockTax.new(transaction_list) }
         stock_taxes.each { |stock_tax| stock_tax.calculate_taxes }
         tax = stock_taxes.map(&:tax)
-
-        # Espera-se que o imposto seja zero devido ao prejuízo aplicado
-         expect(tax).to eq([[[{:tax=>"0.00"}], [{:tax=>"0.00"}]]])
+        expect(tax).to eq([[[{:tax=>"0.00"}], [{:tax=>"0.00"}]]])
       end
     end
   end
